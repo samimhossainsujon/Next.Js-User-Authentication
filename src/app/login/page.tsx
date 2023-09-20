3d8bfcda00ca9aa3ac7ccb967de6e3f7ad3d490e
samim hossain sujon <samimhossainsanto@gmail.com>
2023-09-20 07:20:31 +0600

    code add

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,56 @@
-
-export default function LoginPage() {
-    return (
-        <div>
-            LoginPage
-        </div>
-    );
-};
-
+"use client"
+    import Link from 'next/link';
+    import { useState } from 'react';
+    import { useRouter } from 'next/navigation';
+    import axios from 'axios';
+    
+    export default function LoginPage() {
+        const [user, setUser] = useState({
+            email: "",
+            password: "",            
+        })
+        const onLogin = async () => {
+    
+        }
+    
+        return (
+            <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-black text-white">
+                <h1 >LoginPage</h1>
+                <hr />        
+    
+    
+                <label htmlFor="email">email</label>
+                <input
+                    className='p-3 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+                    id='email'
+                    type='email'
+                    value={user.email}
+                    onChange={(e) => { setUser({ ...user, email: e.target.value }) }}
+                    placeholder='enter your user name'
+                />
+    
+    
+    
+                <label htmlFor="password">password</label>
+                <input
+                    className='p-3 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+                    id='password'
+                    type='password'
+                    value={user.password}
+                    onChange={(e) => { setUser({ ...user, password: e.target.value }) }}
+                    placeholder='enter your user name'
+                />
+    
+                <button
+                    onClick={onLogin}
+                    className="bg-blue-600 p-3 rounded-lg hover:bg-yellow-300 hover:text-black">
+                    Login Now
+                </button>
+                <Link href='/signup'>
+                    visit to login
+                </Link>
+            </div>
+        );
+    };
+    
+    
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,64 @@
+"use client"
+import Link from 'next/link';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 export default function SignupPage() {
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+        username: ""
+    })
+    const onSingup = async () => {
+
+    }
+
     return (
-        <div>
-            <h1>SignupPage</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-black text-white">
+            <h1 >Sign Up</h1>
+            <hr />
+            <label htmlFor="username">UserName</label>
+            <input
+                className='p-3 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+                id='username'
+                type='text'
+                value={user.username}
+                onChange={(e) => { setUser({ ...user, username: e.target.value }) }}
+                placeholder='enter your user name'
+            />
+
+
+            <label htmlFor="email">email</label>
+            <input
+                className='p-3 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+                id='email'
+                type='email'
+                value={user.email}
+                onChange={(e) => { setUser({ ...user, email: e.target.value }) }}
+                placeholder='enter your user name'
+            />
+
+
+
+            <label htmlFor="password">password</label>
+            <input
+                className='p-3 border-gray-400 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+                id='password'
+                type='password'
+                value={user.password}
+                onChange={(e) => { setUser({ ...user, password: e.target.value }) }}
+                placeholder='enter your user name'
+            />
+
+            <button
+                onClick={onSingup}
+                className="bg-blue-600 p-3 rounded-lg hover:bg-yellow-300 hover:text-black">
+                Signup Here
+            </button>
+            <Link href='/login'>
+                visit to login
+            </Link>
         </div>
     );
 };
